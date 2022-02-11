42d3dca75d1a0e34eaa76c1c16f9ab884a0af25c
olemart1n <91548973+olemart1n@users.noreply.github.com>
2022-02-11 13:41:29 +0100

    third commit

--- js/question2.js
+++ js/question2.js
@@ -2,23 +2,29 @@
 // https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=
 const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=fd553c7b21f04a49bd5ac558a3e1af3e";
 const resultsHTML = document.querySelector(".results");
+resultsHTML.innerHTML = "";
 async function getGamesData() {
-    const response = await fetch(url);
-    const result = await response.json();
-    const resultsArray = result.results;
-    for (let i = 0; i < resultsArray.length; i++) {
-        const gameNames = resultsArray[i].name;
-        const gameRating = resultsArray[i].rating;
-        const gameTags = resultsArray[i].tags.length;
-        if (i === 8) {
-            break;
+    try {
+        const response = await fetch(url);
+        const result = await response.json();
+        const resultsArray = result.results;
+        for (let i = 0; i < resultsArray.length; i++) {
+            const gameNames = resultsArray[i].name;
+            const gameRating = resultsArray[i].rating;
+            const gameTags = resultsArray[i].tags.length;
+            if (i === 8) {
+                break;
+            }
+            resultsHTML.innerHTML += `<div class="gameContainer">
+                                        <p class="name">${gameNames}</p>
+                                        <p class="rating">Rating:<b> ${gameRating}</b></p>
+                                        <p class="tags">Amount Of Tags: <b>${gameTags}</b></p>
+                                        </div>
+                                        `;
         }
-        resultsHTML.innerHTML += `<div class="gameContainer">
-                                    <p class="name">${gameNames}</p>
-                                    <p class="rating">Rating:<b> ${gameRating}</b></p>
-                                    <p class="tags">Amount Of Tags: <b>${gameTags}</b></p>
-                                    </div>
-                                    `;
+        
+    } catch (error) {
+        console.log("problems loading ");
     }
 }
 getGamesData()
