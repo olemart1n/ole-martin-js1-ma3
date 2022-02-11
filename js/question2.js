02e1681391f510eb43aa8a8363d6915177dae6e6
olemart1n <91548973+olemart1n@users.noreply.github.com>
2022-02-11 14:12:20 +0100

    fourth commit

--- js/question2.js
+++ js/question2.js
@@ -1,5 +1,6 @@
 // KEY fd553c7b21f04a49bd5ac558a3e1af3e
 // https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=
+/*
 const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=fd553c7b21f04a49bd5ac558a3e1af3e";
 const resultsHTML = document.querySelector(".results");
 resultsHTML.innerHTML = "";
@@ -27,4 +28,4 @@ async function getGamesData() {
         console.log("problems loading ");
     }
 }
-getGamesData()
+getGamesData()*/
